20d57dfaaaa52512a91a09203dbfc3b3c3b2e8c3
cccaaannn <can.kurt.aa@gmail.com>
2022-12-04 00:16:22 +0300

    FileSystemRepository annotated with Repository

--- src/main/java/com/kurtcan/javacore/repository/filesystem/aspect/annotations/FileSystemRepository.java
+++ src/main/java/com/kurtcan/javacore/repository/filesystem/aspect/annotations/FileSystemRepository.java
@@ -1,10 +1,13 @@
 package com.kurtcan.javacore.repository.filesystem.aspect.annotations;
 
+import org.springframework.stereotype.Repository;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@Repository
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface FileSystemRepository {
